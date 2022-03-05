ae7bb82b42a066262c5b33572974634d216aa162
Mostafa <mosi3883@gmail.com>
2022-03-05 23:07:16 +0330

    using theme context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { themeContext } from "../context/theme-contenxt";
 // styles
 
 import "./Navbar.css";
 import Seachbar from "./Seachbar";
 
 function Navbar() {
+  const { color } = useContext(themeContext);
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
